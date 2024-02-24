1666e73aec15ac038244c90cd7b9839a5d8fe236
Caden Schroeder <caden_schroeder@brown.edu>
2024-02-24 17:51:18 -0500

    feat: searching returns correct rows and also finished making the search map mocked data

--- mocked_data/mockedJson.ts
+++ mocked_data/mockedJson.ts
@@ -1,40 +1,59 @@
 const file_map = new Map<string, string[][]>;
 const search_map : Map<string, Map<string, string[][]>> = new Map<string, Map<string, string[][]>>;
 
+/**
+ * Helper method to add the mocked data to maps for mocked CSV functions
+ * @param filepath : string to representing the filepath to map to
+ * @param mockedCSV : mockedCSV data to map words for
+ */
+function addToMaps(filepath : string, mockedCSV : string[][]) {
+    file_map.set(filepath, mockedCSV);
+    const mockedCSVMap : Map<string, string[][]> = new Map<string,string[][]>;
+    for(let i = 0; i < mockedCSV.length; i++){
+        for(const element of mockedCSV[i]){
+            const lowerCaseElement = element.toLowerCase(); //todo is there a cleaner way?
+            const rowsList = mockedCSVMap.get(lowerCaseElement);
+            if(rowsList){
+                if(!rowsList.includes(mockedCSV[i])){
+                    rowsList.push(mockedCSV[i])
+                    mockedCSVMap.set(lowerCaseElement, rowsList);
+                }
+            } else {
+                mockedCSVMap.set(lowerCaseElement, [mockedCSV[i]]);
+            }
+        }
+    }
+    search_map.set(filepath, mockedCSVMap);
+}
+
 const mockedCSV : string[][] = [
     ["Location", "Floors", "Occupants", "Bathrooms"],
     ["Boston", "3", "6", "3"],
     ["California", "1", "1", "1"]
 ]
-file_map.set("data/mockedCSV", mockedCSV);
-const mockedCSVMap : Map<string, string[][]> = new Map<string,string[][]>;
-for(let i = 0; i < mockedCSV.length; i++){
-    for(const element of mockedCSV[i]){
-        mockedCSVMap.get(element);
-        mockedCSVMap.set(element, mockedCSVMap.get(element)?.push(mockedCSV[i]));
-    }
-}
+addToMaps("data/mockedCSV", mockedCSV);
+
 
 const mockedCSVNoHeader : string[][] = [
     ["Boston", "3", "6", "3"],
     ["California", "1", "1", "1"]
 ]
-file_map.set("data/mockedCSVNoHeader", mockedCSVNoHeader);
+addToMaps("data/mockedCSVNoHeader", mockedCSVNoHeader);
 
 const mockedCSVLargeNumbers: string[][] = [
     ["Location", "Floors", "Occupants", "Bathrooms", "Price"],
     ["Boston", "3", "6", "3", "2,000"],
     ["California", "1", "1", "1","1,500"]
 ]
-file_map.set("data/mockedCSVLargeNumbers", mockedCSVLargeNumbers)
+addToMaps("data/mockedCSVLargeNumbers", mockedCSVLargeNumbers);
 
 const mockedCSVSharedAcrossRows: string[][] = [
   ["Location", "Floors", "Occupants", "Bathrooms", "Price"],
   ["Boston", "3", "6", "3", "2,000"],
-  ["California", "1", "1", "1,500"],
+  ["California", "1", "1", "1", "1,500"],
   ["Boston", "1", "2", "4", "1,000"]
 ];
-file_map.set("data/mockedCSVSharedAcrossRows", mockedCSVSharedAcrossRows)
+addToMaps("data/mockedCSVSharedAcrossRows", mockedCSVSharedAcrossRows);
 
 const mockedSearch : string[][] = [
     ["California", "1", "1", "1"]
@@ -63,5 +82,13 @@ export function viewcsv() : string[][] {
 
 //args: 0: keyword; 1: columnIdentifier
 export function searchcsv(args: string[]) : string[][] {
-  return []
+    const wordsToRowsMap = search_map.get(currentCSV);
+    if(wordsToRowsMap){
+        const rowsFound = wordsToRowsMap.get(args[0].toLowerCase()); //TODO add column identifier??
+        if(rowsFound){
+            return rowsFound;
+        }
+    }
+    //todo error handle / throw error
+    return [["Keyword not found"]];
 }
